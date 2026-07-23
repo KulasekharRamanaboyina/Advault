76d5b4af7e597669fd4515abd5d00a0044b0a170
quantiradev <quantiradevelopers@gmail.com>
2026-07-23 16:46:03 +0530

    added cms deployment id

--- Advault-cms/sanity.cli.ts
+++ Advault-cms/sanity.cli.ts
@@ -1,4 +1,4 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
@@ -10,6 +10,8 @@ export default defineCliConfig({
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
+
+    appId: 'a6frfe8lsxzra8s1gbxt44dt',
     autoUpdates: true,
   },
 })

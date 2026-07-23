9d20c60272d0b4c996b94620e2637874ac0ee573
quantiradev <quantiradevelopers@gmail.com>
2026-07-23 16:54:18 +0530

    removed hash router in nav logo

--- Advault-web/src/components/layout/Navbar.tsx
+++ Advault-web/src/components/layout/Navbar.tsx
@@ -4,7 +4,7 @@ export const Navbar: React.FC = () => {
   return (
     <header className="nav" id="main-nav">
       <div className="container nav__inner" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
-        <a href="#home" className="nav__logo">
+        <a href="/" className="nav__logo">
           A<span>dVault</span>
         </a>
       </div>
